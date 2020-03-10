3e3fe71c491e109f313922518f91aaba18a5fc0b
skyclean <skyclean906@gmail.com>
2020-03-10 17:00:37 +0800

    update

--- client/component/Card/CardBetResult.jsx
+++ client/component/Card/CardBetResult.jsx
@@ -44,28 +44,29 @@ const CardBetResult = ({eventInfo, data, t}) => {
     // Spreads
     let SpreadsBets = { home: [], away: [], draw: [] };
 
-    eventInfo.homeBets.map((event) => {
-      if (event.betChoose.includes('Spreads - Home')) {
+    eventInfo.spreadHomeBets.map((event) => {
+      //if (event.betChoose.includes('Spreads - Home')) {
         SpreadsBets.home.push(event);
-      }
+      //}
     });
-    eventInfo.awayBets.map((event) => {
-      if (event.betChoose.includes('Spreads - Away')) {
+    eventInfo.spreadAwayBets.map((event) => {
+      //if (event.betChoose.includes('Spreads - Away')) {
         SpreadsBets.away.push(event);
-      }
+      //}
     });
     
-    eventInfo.drawBets.map((event) => {
-      if (event.betChoose.includes('Spreads - Draw')) {
-        SpreadsBets.draw.push(event);
-      }
-    });
+    // eventInfo.drawBets.map((event) => {
+    //   if (event.betChoose.includes('Spreads - Draw')) {
+    //     SpreadsBets.draw.push(event);
+    //   }
+    // });
 
     const SHomeBetAmount = SpreadsBets.home.reduce((acc, bet) => acc + bet.betValue, 0.0);
     const SAwayBetAmount = SpreadsBets.away.reduce((acc, bet) => acc + bet.betValue, 0.0);
-    const SDrawBetAmount = SpreadsBets.draw.reduce((acc, bet) => acc + bet.betValue, 0.0);
+    //const SDrawBetAmount = SpreadsBets.draw.reduce((acc, bet) => acc + bet.betValue, 0.0);
 
-    totalBet +=  SHomeBetAmount + SAwayBetAmount + SDrawBetAmount;
+    console.log('SpreadBets', SpreadsBets, SHomeBetAmount, SAwayBetAmount);
+    totalBet +=  SHomeBetAmount + SAwayBetAmount; //+ SDrawBetAmount;
 
     // Over / Under
     let over = [];
