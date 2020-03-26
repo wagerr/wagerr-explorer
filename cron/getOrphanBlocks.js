84bee7c5e9f380522926e76e84c9ea501c14f1dc
skyclean <skyclean906@gmail.com>
2020-03-26 11:11:38 +0800

    update

--- cron/getOrphanBlocks.js
+++ cron/getOrphanBlocks.js
@@ -5,19 +5,33 @@ const { exit, rpc } = require('../lib/cron');
 const { forEachSeries } = require('p-iteration');
 const locker = require('../lib/locker');
 const util = require('./util');
-// Models.
-const Block = require('../model/block');
 const TX = require('../model/tx');
-const UTXO = require('../model/utxo');
-const STXO = require('../model/stxo');
-
+const { BigNumber } = require('bignumber.js');
 
 
 async function findOrphanBlock(){
-  const blocks = await Block.find({confirmations: {$lt: 1}});
-  console.log(blocks);
-  exit(0);
-  
+  const address = 'Wm5om9hBJTyKqv5FkMSfZ2FDMeGp12fkTe';
+  const txs = await TX
+  .aggregate([
+    { $match: { $or: [{ 'vout.address': address }, { 'vin.address': address }] } },
+    { $sort: { blockHeight: -1 } },
+  ])
+  .allowDiskUse(true)
+  .exec();
+
+  let i = 0;
+  const sent = txs.filter((tx) => tx.vout[0].address !== 'NON_STANDARD')
+  .reduce((acc, tx) => acc.plus(tx.vin.reduce((a, t) => {
+    i++;
+    if (t.address === address) {
+      return a.plus(BigNumber(t.value));
+    }
+
+    return a;
+  }, BigNumber(0.0))), BigNumber(0.0));
+
+  console.log(i);
+  exit(0);  
 }
 
 findOrphanBlock();
