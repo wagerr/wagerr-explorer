ebd875d9a429643063924a1e070fa69bad3564ba
skyclean <skyclean906@gmail.com>
2020-06-14 00:19:42 +0800

    update for orphan block

--- cron/block.js
+++ cron/block.js
@@ -52,7 +52,7 @@ async function syncBlocks(start, stop, clean = false) {
     const hash = await rpc.call('getblockhash', [height]);
     const rpcblock = await rpc.call('getblock', [hash]);
 
-    if (rpcblock.confirmations < 1) break; 
+    if (rpcblock.confirmations < 5) break; 
     const block = new Block({
       hash,
       height,
