fa9b16c41dec7cce403e2c16411c3ad214b473f8
skyclean906 <skyclean906@gmail.com>
2020-10-14 16:22:18 +0800

    ***disable header

--- client/component/Menu/globalMenuData.js
+++ client/component/Menu/globalMenuData.js
@@ -1,11 +1,11 @@
 export const globalMenuData = (t) => {
   return [
-    { label: t('Explorer'), icon: '/img/uiupdate/header_explorer.png', href: '/' },
-    { label: t('Bet History'), icon: '/img/uiupdate/header_bet_history.png', href: '/bethistory' },
-    { label: t('Betting'), icon: '/img/uiupdate/header_betting.png', href: '/betting' },
-    { label: t('Chain Games'), icon: '/img/uiupdate/header_chain_games.png', href: '/lottos' },
+    { label: t('Explorer'), icon: '/img/uiupdate/header_explorer.png', gicon: '/img/greyicon/header_explorer.png', href: '/' },
+    { label: t('Bet History'), icon: '/img/uiupdate/header_bet_history.png', gicon: '/img/greyicon/header_bet_history.png', href: '/bethistory' },
+    { label: t('Betting'), icon: '/img/uiupdate/header_betting.png', gicon: '/img/greyicon/header_betting.png', href: '/betting' },
+    { label: t('Chain Games'), icon: '/img/uiupdate/header_chain_games.png', gicon: '/img/greyicon/header_chain_games.png', href: '/lottos' },
     {
-      label: t('Help'), icon: '/img/uiupdate/header_help.png', href: '/help',
+      label: t('Help'), icon: '/img/uiupdate/header_help.png', gicon: '/img/greyicon/header_help.png', href: '/help',
       submenu: [
         { label: 'Chart with us', href: '/helpus' },
         { label: 'How to bet here', href: '/help' }
