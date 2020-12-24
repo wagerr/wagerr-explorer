0cc7ebe07533cc09ef5ae15da555316973085bc2
frndxyz <pravin_bhudiya@yahoo.com>
2020-12-24 13:14:52 +0530

    revert daemon installation.

--- docker/node/install.sh
+++ docker/node/install.sh
@@ -40,7 +40,18 @@ apt-get install protobuf-compiler -y
 #cp wagerr-tx /usr/local/bin
 #cd
 
-rm -rf /tmp/wagerr
+# Download latest node and install.
+#echo 'Installing wagerr'
+#wgrlink=`curl -s https://api.github.com/repos/wagerr/wagerr/releases/latest | grep browser_download_url | grep x86_64-linux-gnu.tar.gz | cut -d '"' -f 4`
+#mkdir -p /tmp/wagerr
+#cd /tmp/wagerr
+#curl -Lo wagerr.tar.gz $wgrlink
+#tar -xvf wagerr.tar.gz
+#wgrfolder=`ls | grep wagerr-`
+#cd /tmp/wagerr/$wgrfolder
+#mv ./bin/* /usr/local/bin
+#cd
+#rm -rf /tmp/wagerr
 
 mkdir /root/.wagerr
 
