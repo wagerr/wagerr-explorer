d98835cab473c931b9cefb06f42a839655e0104f
frndxyz <pravin_bhudiya@yahoo.com>
2021-01-20 18:22:11 +0530

    ordering explorer menu items.

--- client/component/Menu/explorerMenuData.js
+++ client/component/Menu/explorerMenuData.js
@@ -1,12 +1,12 @@
 export const genMenuData = (t) => {
   return [
     {label: t('overview'), icon: '/img/home.svg', href: '/'},
+    {label: t('Sports Betting'), icon: '/img/betevent.svg', href: '/betevents'},      
+    {label: t('Parlay Betting'), icon: '/img/betevent.svg', href: '/betparlays'},
     {label: t('movement'), icon: '/img/movement.svg', href: '/movement'},
     // {label: t('top'), icon: '/img/top100.svg', href: '/top'},
     {label: t('masternode'), icon: '/img/masternodes.svg', href: '/masternode'},
     // {label: t('governance'), icon: '/img/governance.svg', href: '/governance'},
-    {label: t('Sports Betting'), icon: '/img/betevent.svg', href: '/betevents'},      
-    {label: t('Parlay Betting'), icon: '/img/betevent.svg', href: '/betparlays'},      
     {label: t('connections'), icon: '/img/connections.svg', href: '/peer'},
     {label: t('statistics'), icon: ' /img/statistics.svg', href: '/statistics'},
     {label: t('coinInfo'), icon: '/img/coininfo.svg', href: '/coin'},
